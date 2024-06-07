8c58667bbb1e86692fefd4754adeca06c356dee4
Matheus Medeiros <mtmedeirosgs@gmail.com>
2024-06-07 11:30:01 -0700

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Button } from "./components/ui/button"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./routes"
+
 import "./global.css"
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen flex-col">
-      <h1>Hello World</h1>
-      <Button>Shadcn Ui Button</Button>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
