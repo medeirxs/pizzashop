93eecdd80e1e8dc74b641b6205685915e17cf0c0
Matheus Medeiros <mtmedeirosgs@gmail.com>
2024-06-11 10:11:51 -0700

    cleaning css unused tags

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,19 +3,11 @@ import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input";
 
-//<form className="flex flex-col gap-4">
-//<div className="">
-//<Label htmlFor="email">Seu e-mail</Label>
-//<Input id="email" type="email" placeholder="m@example.com" />
-//</div>
-//<Button type="submit" className=" w-full">Acessar</Button>
-//</form>
-
 export function SignIn(){
   return(
     <>
     <Helmet title="Login" />
-      <div className="space-y-4 w-[400px] h-96 p-8 flex flex-col justify-center rounded-lg drop-shadow-xl">
+      <div className="space-y-6 w-[400px] p-8 flex flex-col">
         <div className="flex flex-col items-center">
           <span className="text-3xl font-bold tracking-tight">Acessar Painel</span>
           <span className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro!</span>
