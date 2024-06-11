4b52ebd20b0018b68f5457fa91a7e79292696969
Matheus Medeiros <mtmedeirosgs@gmail.com>
2024-06-11 10:09:00 -0700

    add sign in page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom'
+import { PizzaIcon } from 'lucide-react'
 
 export function AuthLayout(){
   return(
-    <div>
-      <h1>Autenticaçõa</h1>
-
-      <div>
-        <Outlet/>
+    <div className='flex min-h-screen'>
+      <div className='flex flex-col items-center justify-center mx-auto px-auto gap-10'>
+        <div className='flex items-center gap-2 text-3xl font-medium text-foreground'>
+          <PizzaIcon className='h-9 w-9'/>
+          pizza.shop
+        </div>
+      
+        <div className='flex justify-center items-center'>
+          <Outlet/>
+        </div>
       </div>
     </div>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,11 +1,35 @@
 import { Helmet } from "react-helmet-async";
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input";
+
+//<form className="flex flex-col gap-4">
+//<div className="">
+//<Label htmlFor="email">Seu e-mail</Label>
+//<Input id="email" type="email" placeholder="m@example.com" />
+//</div>
+//<Button type="submit" className=" w-full">Acessar</Button>
+//</form>
 
 export function SignIn(){
   return(
     <>
     <Helmet title="Login" />
-    <div>sign-in</div>
-    </>
-    
+      <div className="space-y-4 w-[400px] h-96 p-8 flex flex-col justify-center rounded-lg drop-shadow-xl">
+        <div className="flex flex-col items-center">
+          <span className="text-3xl font-bold tracking-tight">Acessar Painel</span>
+          <span className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro!</span>
+        </div>
+
+        <form className="flex flex-col gap-2">
+          <Label htmlFor="email">Seu e-mail</Label>
+          <Input id="email" type="email" placeholder="m@example.com"/>
+
+          <Button type="submit">Acessar</Button>
+        </form>
+
+        <span className='text-sm flex flex-col items-center text-muted-foreground'> Painel do parceiro &copy; pizza.shop</span>
+      </div>
+    </> 
   )
 }
